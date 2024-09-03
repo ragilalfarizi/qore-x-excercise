dffb47a62b518efde34f7a9ec7351abe310deae9
Ragil Alfarizi <ragilalfarizi23@gmail.com>
2024-09-03 14:52:50 +0700

    feat: read bme280 data

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,37 @@
+#include <Adafruit_BME280.h>
+#include <Adafruit_Sensor.h>
 #include <Arduino.h>
+#include <SPI.h>
+#include <Wire.h>
+
+#include "bme280_helper.h"
+
+#define DEFAULT_DELAY 1000
+
+/* OBJECT INIT */
+Adafruit_BME280 bme;
+
+/* GLOBAL VARIABLES INIT */
+BME280Data      bmeData;
 
 void setup() {
-  // put your setup code here, to run once:
+  /* SERIAL INIT */
   Serial.begin(115200);
+  while (!Serial);  // time to get serial running
+  Serial.println(F("BME280 test"));
+
+  /* BME280 INIT */
+  unsigned status;
+  status = bme.begin(0x76);
+  if (!status) {
+    Serial.println(
+        "Could not find a valid BME280 sensor, check wiring, address, sensor "
+        "ID!");
+  }
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
-  Serial.println("Hello World!");
-  delay(1000);
+  /* PRINT BME280 DATA */
+  printBME280Values();
+  delay(DEFAULT_DELAY);
 }
