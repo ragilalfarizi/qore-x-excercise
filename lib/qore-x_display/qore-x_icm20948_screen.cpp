fa84c37e176017cb7ebfd97517b02330b545ceb5
Ragil Alfarizi <ragilalfarizi23@gmail.com>
2024-09-12 12:15:34 +0700

    "feat: change each screen to image"

--- lib/qore-x_display/qore-x_icm20948_screen.cpp
+++ lib/qore-x_display/qore-x_icm20948_screen.cpp
@@ -5,7 +5,13 @@ lv_obj_t *createICM2094Screen() {
 
   /* CREATE MAIN CONTENT */
   // TODO: break this down into pieces, not just image
-  lv_obj_set_style_bg_color(screen, lv_palette_main(LV_PALETTE_YELLOW), 0);
+  // lv_obj_set_style_bg_color(screen, lv_palette_main(LV_PALETTE_YELLOW), 0);
+
+  LV_IMAGE_DECLARE(screen_icm);
+
+  lv_obj_t *Img = lv_image_create(screen);
+  lv_image_set_src(Img, &screen_icm);
+  lv_obj_align(Img, LV_ALIGN_CENTER, 0, 0);
 
   /* CREATE BOTTOM MENU */
   createBottomSwitch(screen);
